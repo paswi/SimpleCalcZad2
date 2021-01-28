a356ce8f0d33e540134dd14e46dfc94aec61d713
paswi <pawels148@gmail.com>
2021-01-28 21:07:45 +0100

    layout works to be done: merge with "szablon" add log features onCreate()

--- app/src/main/java/com/example/simplecalc/MainActivity.java
+++ app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 /*
+
         Button buttonSimple = findViewById(R.id.buttonSimpleFragment);
         Button buttonProg = findViewById(R.id.buttonProgrammerFragment);
 */
